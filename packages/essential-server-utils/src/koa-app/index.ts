ee96469298bd2b60f0d120ed9b75693ce7875511
Sanyam Arya <er.sanyam.arya@gmail.com>
2023-08-10 11:17:34 +0200

    Added RateLimit to koa

--- packages/essential-server-utils/src/koa-app/index.ts
+++ packages/essential-server-utils/src/koa-app/index.ts
@@ -4,11 +4,20 @@ import bodyParser from 'koa-body'
 import * as cors from 'koa2-cors'
 import requestLoggerMiddleware from './middleware/request-logger'
 
+import { RateLimit, RateLimitOptions } from 'koa2-ratelimit'
+
 /* Create a new Koa instance, and add a couple of middleware. */
-export const koaApp = async (): Promise<Koa> => {
+export const koaApp = async (rateLimitOptions?: Partial<RateLimitOptions>): Promise<Koa> => {
   logger.info('--------> Creating Koa app <--------')
   const app = new Koa()
   app.use(requestLoggerMiddleware())
+
+  rateLimitOptions = rateLimitOptions || {
+    interval: { min: 15 },
+    max: 100,
+    message: 'Sometimes You Just Have to Slow Down.',
+  }
+  app.use(RateLimit.middleware(rateLimitOptions))
   app.use(cors({ origin: '*' }))
   app.use(
     bodyParser({
